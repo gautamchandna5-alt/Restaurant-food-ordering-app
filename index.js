ab1791385c8cbee4a222c669d8a31dc576dc37cd
gautamchandna5-alt <gautamchandna5@gmail.com>
2026-08-05 13:11:05 +0530

    rendering the menu

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ import { menuArray } from './data.js';
 
 const menu = document.getElementById('menu');
 
+const renderMenu = menuArray.map((item) => {
+    return `
+        <div class="menu-items">
+                <div class="menu-item">
+                    <div class="item-graphic">${item.emoji}</div>
+                    <div class="item-details">
+                        <h3 class="item-name">${item.name}</h3>
+                        <p class="item-ingredients">${item.ingredients.join(', ')}</p>
+                        <p class="item-price">$${item.price}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+})
+menu.innerHTML = renderMenu.join('');
+
