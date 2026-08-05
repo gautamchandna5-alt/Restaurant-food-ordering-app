fbfc7422f5242c3c98eb4d3ad45a1b500fc4f3ce
gautamchandna5-alt <gautamchandna5@gmail.com>
2026-08-05 13:32:32 +0530

    added the add btn

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ const renderMenu = menuArray.map((item) => {
     return `
         <div class="menu-items">
                 <div class="menu-item">
-                    <div class="item-graphic">${item.emoji}</div>
+                    <div class="item-graphic">
+                    ${item.emoji}
+                    </div>
                     <div class="item-details">
                         <h3 class="item-name">${item.name}</h3>
                         <p class="item-ingredients">${item.ingredients.join(', ')}</p>
                         <p class="item-price">$${item.price}</p>
                     </div>
+                    <button class="add-btn" data-item-id="${item.id}">+</button>
                 </div>
-            </div>
         </div>
     `;
 })
